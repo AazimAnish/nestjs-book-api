e68f9d6e43ed6ec4f92cdb4e2cb888e8cc98ed47
AazimAnish <aazimanish1@gmail.com>
2024-05-16 03:45:22 +0530

    getallbooks GET done

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { BooksService } from './app.service';
+import { Book } from './FakeDatabase';
 
-@Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
+@Controller('books')
+export class BooksController {
+  constructor(private readonly booksService: BooksService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getAllBooks(): Book[] {
+    return this.booksService.getAllBooks();
   }
 }
